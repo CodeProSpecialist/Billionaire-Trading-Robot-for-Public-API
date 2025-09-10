6f9d5ed744022c6ae8444e0592de3a4fa6f071b9
CodeProSpecialist <111866070+CodeProSpecialist@users.noreply.github.com>
2025-09-10 05:22:09 -0500

    Update and rename get-account-id.py to get-new-cookie-token.py

---
+++ get-new-cookie-token.py
@@ -0,0 +1,49 @@
+import os
+import requests
+import uuid
+
+load_dotenv()
+
+# below is correct API secret key reading code
+# secret = os.getenv("YOUR_SECRET_KEY")
+
+# Permanent secret
+secret = os.getenv("YOUR_SECRET_KEY")
+if not YOUR_SECRET_KEY:
+    raise ValueError("Please set YOUR_SECRET_KEY in your environment.")
+
+# Authorization
+url = "https://api.public.com/userapiauthservice/personal/access-tokens"
+headers = {
+    "Content-Type": "application/json"
+}
+
+request_body = {
+  "validityInMinutes": 1440,
+  "secret": secret
+}
+
+response = requests.post(url, headers=headers, json=request_body)
+access = response.json()["accessToken"]
+
+# Account Id
+url = "https://api.public.com/userapigateway/trading/account"
+headers = {
+    "Authorization": f"Bearer {access}",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+accountId = response.json()["accounts"][0]["accountId"]
+
+# Portfolio
+url = f"https://api.public.com/userapigateway/trading/{accountId}/portfolio/v2"
+headers = {
+    "Authorization": f"Bearer {access}",
+    "Content-Type": "application/json"
+}
+
+response = requests.get(url, headers=headers)
+data = response.json()
+data.pop("accountId", None)
+print(data)
